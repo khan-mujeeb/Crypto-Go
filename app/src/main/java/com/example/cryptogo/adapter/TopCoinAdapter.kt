4362a7aa0d3ee8bcb24000aee81f2821b8c17cbb
Mujeeb <mujeebkhan1831@gmail.com>
2022-12-23 23:27:45 +0530

    topcoin data fetch from api

---
+++ app/src/main/java/com/example/cryptogo/adapter/TopCoinAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.cryptogo.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.cryptogo.R
+import com.example.cryptogo.model.CryptoCurrency
+
+class TopCoinAdapter(val context: Context, var list: List<CryptoCurrency>): RecyclerView.Adapter<TopCoinAdapter.TopCoinViewHolder>() {
+
+    inner class TopCoinViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        var coinImage: ImageView = view.findViewById(R.id.coin_image)
+        var coinName:  TextView = view.findViewById(R.id.name)
+        var coinPrice: TextView = view.findViewById(R.id.coinPrice)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopCoinViewHolder {
+        var view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.top_coin_itemview, parent, false)
+        return TopCoinViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TopCoinViewHolder, position: Int) {
+        holder.coinName.text = list[position].name
+
+        Glide.with(context)
+            .load("https://s2.coinmarketcap.com/static/img/coins/64x64/${list[position].id}.png")
+            .thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.coinImage)
+
+        val change = list[position].quotes[0].percentChange24h
+        if (change > 0) {
+            holder.coinPrice.setTextColor(context.resources.getColor(R.color.green))
+            holder.coinPrice.text = "+${String.format("%.2f",change)}"
+        } else {
+            holder.coinPrice.setTextColor(context.resources.getColor(R.color.red))
+            holder.coinPrice.text = "${String.format("%.2f",change)}"
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- app/src/main/java/com/example/cryptogo/home/HomeFragment.kt
+++ app/src/main/java/com/example/cryptogo/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cryptogo.home
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,16 +11,20 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.cryptogo.R
+import com.example.cryptogo.adapter.TopCoinAdapter
 import com.example.cryptogo.adapter.ViewPagerAdapter
 import com.example.cryptogo.api.ApiInterface
 import com.example.cryptogo.api.ApiUtlis
 import com.example.cryptogo.databinding.FragmentHomeBinding
+import com.example.cryptogo.model.CryptoCurrency
 import com.example.cryptogo.ui.TopLoserFragment
 import com.example.cryptogo.ui.TopgainerFragment
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
+    lateinit var list: List<CryptoCurrency>
     private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,15 +33,29 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
         switchFragment()
+
         getResponce()
+
+
+
         return binding.root
     }
 
-    private fun getResponce() {
 
+
+    private fun getResponce() {
+//        activity?.runOnUiThread(Runnable {
+//        })
         lifecycleScope.launch(Dispatchers.IO) {
             val result = ApiUtlis.getInstance().create(ApiInterface::class.java).getMarketData()
-            println("mujeeb ${result.body()!!.data.cryptoCurrencyList}")
+            list = result.body()!!.data.cryptoCurrencyList
+            println("mujeeb $list")
+            try {
+                binding.topCoinsRc.adapter = TopCoinAdapter(requireContext(), list)
+            } catch (e: Throwable) {
+                // handle exception
+            }
+
         }
     }
 
