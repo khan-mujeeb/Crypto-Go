d887d9eafed193dbef502234fcb0fcfb65342065
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-01-08 19:08:58 +0530

    search functionality added

--- app/src/main/java/com/example/cryptogo/MainActivity.kt
+++ app/src/main/java/com/example/cryptogo/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.cryptogo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cryptogo.databinding.ActivityMainBinding
@@ -19,15 +19,21 @@ class MainActivity : AppCompatActivity() {
         // bottomnav bar
         init()
 
-        binding.searchEditText.setOnClickListener{
-            val navController: NavController = findNavController(this, R.id.host)
-            navController.navigateUp()
-            navController.navigate(R.id.marketFragment)
-        }
+//        binding.searchEditText.setOnClickListener{
+//            changeFragment()
+//        }
 
-        }
-    fun init() {
-        val navControl = findNavController(findViewById(R.id.host))
-        binding.bottomNavBar.setupWithNavController(navControl)
     }
+
+//    private fun changeFragment() {
+//        val navController: NavController = findNavController(this, R.id.host)
+//        navController.navigateUp()
+//        navController.navigate(R.id.marketFragment)
+////        binding.searchEditText.visibility = View.GONE
+//    }
+
+    fun init() {
+            val navControl = findNavController(findViewById(R.id.host))
+            binding.bottomNavBar.setupWithNavController(navControl)
+        }
 }
--- app/src/main/java/com/example/cryptogo/home/MarketFragment.kt
+++ app/src/main/java/com/example/cryptogo/home/MarketFragment.kt
@@ -2,6 +2,8 @@ package com.example.cryptogo.home
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +16,7 @@ import com.example.cryptogo.adapter.TopGainerAdapter
 import com.example.cryptogo.api.ApiInterface
 import com.example.cryptogo.api.ApiUtlis
 import com.example.cryptogo.databinding.FragmentMarketBinding
+import com.example.cryptogo.model.CryptoCurrency
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -21,33 +24,78 @@ import java.util.*
 
 class MarketFragment : Fragment() {
 
+    private lateinit var adapter: MarketAdapter
     private lateinit var binding: FragmentMarketBinding
+    var list = listOf<CryptoCurrency>()
+    private lateinit var searchText: String
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMarketBinding.inflate(layoutInflater)
-        // making search edit text visible
-        val search = activity?.findViewById<EditText>(R.id.search_edit_text)
-        search?.visibility = View.VISIBLE
+
+        // making search edit text invisible
+
 
         getResponce()
+        searchCoin()
+
+
         // Inflate the layout for this fragment
         return binding.root
     }
 
     private fun getResponce() {
         binding.loading.visibility = View.VISIBLE
+
+        adapter = MarketAdapter(requireContext(),list)
+        binding.marketRc.adapter = adapter
+
         lifecycleScope.launch(Dispatchers.IO) {
             val result = ApiUtlis.getInstance().create(ApiInterface::class.java).getMarketData()
             if(result.body()!=null) {
                 withContext(Dispatchers.Main) {
-                    var list = result.body()!!.data.cryptoCurrencyList
+                    list = result.body()!!.data.cryptoCurrencyList
+                    adapter.updateDataList(list)
                     binding.loading.visibility = View.INVISIBLE
                     binding.marketRc.visibility = View.VISIBLE
-                    binding.marketRc.adapter = MarketAdapter(requireContext(),list)
                 }
             }
         }
+    }
+
+    public fun searchCoin() {
+
+        binding.searchEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                searchText = p0.toString().toLowerCase()
+                updateRecyclerView()
+            }
+
+        })
+
+    }
+
+    private fun updateRecyclerView() {
+        var data = ArrayList<CryptoCurrency>()
+        for (item in list) {
+            val coinName = item.name.toString().toLowerCase()
+            val coinSymbol = item.symbol.toString().toLowerCase()
+
+            if (coinName.contains(searchText) || coinSymbol.contains(searchText)) {
+                data.add(item)
+            }
+        }
+        adapter.updateDataList(data)
     }
 }
